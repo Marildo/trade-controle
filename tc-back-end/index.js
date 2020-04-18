eb7999bcb3bccd2f298a37d2039ed20539a76b38
marildocesar <marildocesar@hotmail.com>
2020-04-17 21:08:00 -0300

    Configurando Apollo Server

--- tc-back-end/index.js
+++ tc-back-end/index.js
@@ -1,1 +1,24 @@
-console.log('começando back-end')
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+    type Query{
+        helo:String
+    }
+`
+
+const resolvers = {
+    Query:{
+        helo(){
+            return `Api rodando!  ${new Date}`
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(({ url }) => {
+    console.log(`Executando em ${url}`)
+})
