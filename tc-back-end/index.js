8732f83b3b4020fadcb407e2a2f21ec2870484da
marildocesar <marildocesar@hotmail.com>
2020-04-18 09:46:58 -0300

    Criando Query e Resolver de Carteira

--- tc-back-end/index.js
+++ tc-back-end/index.js
@@ -1,15 +1,30 @@
 const { ApolloServer, gql } = require('apollo-server')
 
 const typeDefs = gql`
+
+    type Carteira{
+        id: ID
+        nome: String
+    }
+
     type Query{
         helo:String
+        carteira:Carteira
     }
+
 `
 
 const resolvers = {
-    Query:{
-        helo(){
+    Query: {
+        helo() {
             return `Api rodando!  ${new Date}`
+        },
+
+        carteira() {
+            return {
+                id: '12',
+                nome: 'Modal'
+            }
         }
     }
 }
