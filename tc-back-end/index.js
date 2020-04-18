a7571406354d95b0e69a226bd1de28a78efc7301
marildocesar <marildocesar@hotmail.com>
2020-04-18 09:59:02 -0300

    Criando Query e Resolver de Acao

--- tc-back-end/index.js
+++ tc-back-end/index.js
@@ -3,13 +3,22 @@ const { ApolloServer, gql } = require('apollo-server')
 const typeDefs = gql`
 
     type Carteira{
-        id: ID
-        nome: String
+        id: ID!
+        nome: String!
+    }
+
+    type Acao {
+        id:ID!
+        sigla:String!
+        empresa:String!
+        cotacao:Float
     }
 
     type Query{
         helo:String
         carteira:Carteira
+
+        acao:Acao
     }
 
 `
@@ -24,6 +33,15 @@ const resolvers = {
             return {
                 id: '12',
                 nome: 'Modal'
+            }
+        },
+
+        acao() {
+            return {
+                id:10,
+                sigla:'LCAM3',
+                empresa: 'Locamerica',
+                cotacao: '18.90'
             }
         }
     }
