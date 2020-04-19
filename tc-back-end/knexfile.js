9396f00d56fc660375b48f6d916e4521965c6177
marildocesar <marildocesar@hotmail.com>
2020-04-19 10:48:03 -0300

    Configurando knexfile.js

--- tc-back-end/knexfile.js
+++ tc-back-end/knexfile.js
@@ -2,6 +2,21 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 module.exports = {
+  client: 'postgresql',
+  connection: {
+    host: process.env.APP_DB_HOST,
+    port: process.env.APP_DB_PORT,
+    database: process.env.APP_DB_NAME,
+    user: process.env.APP_DB_USER,
+    password: process.env.APP_DB_PASSWORD,
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
 
 /*
   development: {
