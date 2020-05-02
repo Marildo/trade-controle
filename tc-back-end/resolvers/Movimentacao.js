753790694ffa7bcbb0168913ab77449e1d79a94c
marildocesar <marildocesar@hotmail.com>
2020-05-02 20:01:23 -0300

    Implementando resolver do tipo de lancamento em movimentacao

--- tc-back-end/resolvers/Movimentacao.js
+++ tc-back-end/resolvers/Movimentacao.js
@@ -1,6 +1,12 @@
+const { tiposLancamentos } = require('../model/enunsModel')
 
 module.exports = {
-    dataMovimentacao(movimentacao){        
-        return  movimentacao.data_movimentacao
+    dataMovimentacao(movimentacao) {
+        return movimentacao.data_movimentacao
+    },
+
+    tipoLancamento(movimentacao) {
+        const tipo = tiposLancamentos().filter(i => i.key == movimentacao.tipo)
+        return tipo[0]
     }
 }
