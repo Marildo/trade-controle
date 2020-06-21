c0cea44c2b92914a5b84d70cfe4faba7174cb4fc
unknown <marildocesar@hotmail.com>
2020-06-21 16:42:01 -0300

    Implementando testes de movimentacaoModel

--- tc-back-end/tests/helpers.js
+++ tc-back-end/tests/helpers.js
@@ -1,4 +1,10 @@
 const chai = require("chai")
 const expect = chai.expect
+const should = chai.should()
+const assert = chai.assert
 
-module.exports = expect
+module.exports = {
+    expect,
+    should,
+    assert
+}
--- tc-back-end/tests/model/movimentacaoModel.test.js
+++ tc-back-end/tests/model/movimentacaoModel.test.js
@@ -1,33 +1,73 @@
-const  expect  = require('../helpers')
-const  movimentacaoModel = require('../../model/movimentacaoModel')
+const { expect, assert } = require('../helpers')
+const movimentacaoModel = require('../../model/movimentacaoModel')
 
 describe('Movimentacao Model', () => {
   describe('#Save', () => {
-    it('Save: Deve salvar e retornar object salvo com id', () => {
-      expect(20).to.equal(260)
+    it('Save: Deve salvar e retornar object salvo com id', function (done) {
+      const mov = {
+        data_movimentacao: new Date(),
+        tipo: 1,
+        valor: 40,
+        descricao: 'Save test',
+        carteira_id: 1,
+      }
+
+      movimentacaoModel
+        .save(mov)
+        .then((resp) => assert.isNumber(resp.id))
+        .then(done())
     })
   })
 
   describe('#FindById', () => {
-    it('Deve retorna pelo id', () => {
-      expect(20).to.equal(260)
+    const id = 10
+    it('Deve retorna pelo id', function (done) {
+      movimentacaoModel
+        .findById(id)
+        .then((resp) => expect(resp.id).to.equal(id))
+        .then(done())
     })
 
     it('Deve retorna undefined', (done) => {
-      movimentacaoModel.findById(30466)
-       .then(resp => {
-         expect(resp).to.equal(undefined)
-         done()
-       })
-       .catch(error => done(error))
+      movimentacaoModel
+        .findById(30466)
+        .then((resp) => expect(resp).to.equal(undefined))
+        .then(done())
     })
   })
 
-  it('FindByIdCarteira: Deve retorna todas as movimentacoes pelo id da carteira', () => {
-    expect(20).to.equal(260)
+  describe('#FindByIdCarteira', () => {
+    it('FindByIdCarteira: Deve retorna todas as movimentacoes pelo id da carteira', function (done) {
+      const idCarteira = 1
+      movimentacaoModel
+        .findByIdCarteira(idCarteira)
+        .then((resp) =>
+          expect(resp.filter((i) => i.carteira_id !== idCarteira).to.be.empty)
+        )
+        .then(done())
+    })
+
+    it('FindByIdCarteira: Deve retorna []', function (done) {
+      const idCarteira = 1544
+      movimentacaoModel
+        .findByIdCarteira(idCarteira)
+        .then((resp) => expect(resp.to.be.empty))
+        .then(done())
+    })
   })
 
-  it('DeleteById: Deve deletar e retornar  sucessos(1)', () => {
-    expect(20).to.equal(260)
+  describe('#DeleteById', () => {
+    it('DeleteById: Deve deletar e retornar sucesso(1)', function (done) {
+        movimentacaoModel
+        .deleteById(20)
+        .then(resp => expect(resp).to.equal(1) )
+        .then(done())
+    })
+    it('DeleteById: Deve deletar e retornar not falha(0)',  function (done) {
+      movimentacaoModel
+      .deleteById(20)
+      .then(resp => expect(resp).to.equal(0) )
+      .then(done())
+    })
   })
 })
