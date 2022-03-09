a08c3c6c626de5533343444428aa027b98808a4b
Marildo Cesar <marildocesar@hotmail.com>
2022-03-09 17:29:09 -0300

    Feature: Cliendo template

---
+++ trade_controler_django/dashboard/static/js/main.js
@@ -0,0 +1,9 @@
+function toggleNav() {
+    const nav = document.getElementsByClassName("nav-list")[0]
+    const aClass = "active-nav"
+    if (nav.classList.contains(aClass)){
+        nav.classList.remove(aClass)
+    } else {
+        nav.classList.add(aClass)
+    }        
+  }
