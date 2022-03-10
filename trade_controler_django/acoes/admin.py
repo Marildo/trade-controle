7cc76c62ce10553b97eedffe877db3f3c0545243
Marildo Cesar <marildocesar@hotmail.com>
2022-03-10 07:38:17 -0300

    Feature: Criando model para cadastro de acoes

--- trade_controler_django/acoes/admin.py
+++ trade_controler_django/acoes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Acao, Setor, SubSetor, Segmento
+
 # Register your models here.
+admin.site.register(Acao)
+admin.site.register(Setor)
+admin.site.register(SubSetor)
+admin.site.register(Segmento)
--- trade_controler_django/acoes/models.py
+++ trade_controler_django/acoes/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Segmento(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=127)
+
+    def __str__(self):
+        return f'{self.id} - {self.nome}'
+
+class SubSetor(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=127)
+
+    def __str__(self):
+        return f'{self.id} - {self.nome}'
+
+
+class Setor(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=127)
+
+    def __str__(self):
+        return f'{self.id} - {self.nome}'
+
+
+class Acao(models.Model):
+    id = models.IntegerField(primary_key=True)
+    codigo = models.CharField(max_length=6)
+    nome = models.CharField(max_length=60)
+    cotacao = models.DecimalField(decimal_places=2, max_digits=10)
+    setor = models.ForeignKey(Setor, on_delete=models.DO_NOTHING)
+    subsetor = models.ForeignKey(SubSetor, on_delete=models.DO_NOTHING)
+    segmento = models.ForeignKey(Segmento, on_delete=models.DO_NOTHING)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.codigo} - {self.nome}'
