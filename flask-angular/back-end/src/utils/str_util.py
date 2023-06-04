a1f5dcdec0c6bcf7d0e7866b559bcd254b0cdb8d
Marildo Cesar <marildocesar@hotmail.com>
2023-06-04 09:27:29 -0300

    Feature: Validando BM&F 2022

--- flask-angular/back-end/src/utils/str_util.py
+++ flask-angular/back-end/src/utils/str_util.py
@@ -7,6 +7,8 @@ from re import sub
 
 def str_to_float(value: str) -> float:
     value = value.replace('.', '').replace(',', '.')
+    if not value.strip():
+        return 0.0
     return float(value)
 
 
